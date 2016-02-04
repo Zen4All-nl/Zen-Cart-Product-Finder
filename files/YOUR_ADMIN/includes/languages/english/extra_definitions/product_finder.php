1b23e3f9d551a4498c9773c89898f110b2eddd53
Zen4All <info@zen4all.nl>
2016-02-04 10:35:04 +0100

    Added auto installer, and module settings

---
+++ files/YOUR_ADMIN/includes/languages/english/extra_definitions/product_finder.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * 
+ * @package checkout_reloaded
+ * @copyright Copyright 2003-2015 ZenCart.Codes a Pro-Webs Company
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @filename checkout_reloaded.php
+ * @file created 2015-02-10 10:20:48 PM
+ * 
+ */
+
+define('BOX_CONFIGURATION_PRODUCT_FINDER', "Product Finder Configuration");
