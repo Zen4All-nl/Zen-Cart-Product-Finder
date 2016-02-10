0f4975b61a8cdd521b0db016e69f316ce9d6cf88
Zen4All <info@zen4all.nl>
2016-02-10 16:57:00 +0100

    update for sidebox to new code

--- 1_Installation_files/includes/modules/sideboxes/product_finder.php
+++ 1_Installation_files/includes/modules/sideboxes/product_finder.php
@@ -13,6 +13,21 @@
 $show_product_finder = true;
 
 if ($show_product_finder == true) {
+
+  $pf_base_category = PRODUCT_FINDER_PARENT_ID; //USER defined base category
+  $pf_category_depth = PRODUCT_FINDER_CATEGORY_DEPTH; //USER defined base category
+//check the current page location and separate the category and subcategory cPaths into an array
+  if (isset($_GET['cPath']) && $_GET['cPath'] != '') {
+    $pf_cPaths = explode("_", $_GET['cPath']);
+  }
+
+//check if the current page is a possible Product Finder category to pre-populate the dropdowns with the current category id/values
+  if ((int)$pf_cPaths['0'] == $pf_base_category && count($pf_cPaths) == $pf_category_depth) {
+//do nothing
+  } else {//otherwise it is some other category 
+    unset($pf_cPaths); //this location is NOT a Product Finder category so do not pre-populate the dropdowns
+  }
+
   require($template->get_template_dir('tpl_product_finder.php', DIR_WS_TEMPLATE, $current_page_base, 'sideboxes') . '/tpl_product_finder.php');
   $title = BOX_HEADING_PRODUCT_FINDER;
   $title_link = false;
--- 1_Installation_files/includes/templates/YOUR_TEMPLATE_NAME/sideboxes/tpl_product_finder.php
+++ 1_Installation_files/includes/templates/YOUR_TEMPLATE_NAME/sideboxes/tpl_product_finder.php
@@ -1,87 +1,75 @@
 <?php
 
 /**
- * blank sidebox - allows a blank sidebox to be added to your site
+ * Product Finder sidebox
  *
  * @package templateSystem
- * @copyright 2007 Kuroi Web Design
  * @copyright Portions Copyright 2003-2007 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: blank_sidebox.php 2007-05-26 kuroi $
+ * @version $Id: product_finder.php 2010-08-05
  */
 $content = '';
-$content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent">';
+$content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent">' . "\n";
 //<!--BOF Product Finder-->
-$content .= zen_draw_form('ymm_sidebox', '');
-$content .= '<div>' . "\n" . '<div>';
-
-$model1[] = array('id' => '', 'text' => TEXT_PLEASE_SELECT);
-if (isset($_POST['cPath1'])) {
-  $value = $_POST['cPath1'];
-}
-$content .= '<span id="pf_title">' . strip_tags(TEXT_FIND_ALL_PRODUCTS) . '</span>';
-$content .= '<span class="pf_selectbox_name">' . TEXT_MAKE . '</span>' . zen_draw_pull_down_menu('cPath1', array_merge($model1, zen_get_category_tree()), $value, 'id="cPath" class="pf_selectbox_text"');
-$content .= '</div>' . "\n";
-$content .= '<div class="pf_noscript">';
-$content .= '<noscript>';
-$content .= zen_image_submit(PF_NOSCRIPT_SUBMIT, PF_NOSCRIPT_SUBMIT_ALT);
-if ($_POST['cPath1'] != '') {
-  $value = $_POST['cPath1'];
-  $dropdownArray = zen_get_category_tree($value);
-}
-$content .= '</noscript>';
-$content .= '</div>' . "\n";
-$content .= '<div>';
-$modulePath = DIR_WS_CATALOG;
-$content .= zen_draw_hidden_field('basemodulepath', $modulePath, 'id="basemodulepath" ');
-$model = array();
-$model[] = array('id' => '', 'text' => TEXT_PLEASE_SELECT);
-if ($_POST['cPath1'] != '') {
-  $model = array_merge($model, $dropdownArray);
-}
-if (isset($_POST['select_model'])) {
-  $value = $_POST['select_model'];
-}
-$content .= '<span class="pf_selectbox_name">' . TEXT_MODEL . '</span>';
-$content .= zen_draw_pull_down_menu('select_model', $model, $value, 'id="select_model" class="pf_selectbox_text"');
-$content .= '</div>' . "\n";
-$content .= '<div class="pf_noscript">';
-$content .= '<noscript>';
-if ($_POST['select_model'] != '') {
-  $value = $_POST['select_model'];
-  $dropdownArray = zen_get_category_tree($value);
-}
-$content .= zen_image_submit(PF_NOSCRIPT_SUBMIT, PF_NOSCRIPT_SUBMIT_ALT);
-$content .= '</noscript>';
-$content .= '</div>' . "\n";
-$content .= '<div>';
-$year = array();
-$year[] = array('id' => '',
-  'text' => TEXT_PLEASE_SELECT
-);
-if ($_POST['select_model'] != '') {
-  $year = array_merge($year, $dropdownArray);
-}
-if (isset($_POST['cPath'])) {
-  $value = $_POST['cPath'];
-}
-if ($_POST['select_model'] != '') {
-  $content .= '<span class="pf_selectbox_name">' . TEXT_YEAR . '</span>';
-  $content .= zen_draw_pull_down_menu('cPath', $year, $value, 'id="select_year" class="pf_selectbox_text"');
+if (isset($_POST['pf_dd1']) && $_POST['pf_dd1'] > 0 && isset($_POST['pf_dd2']) && $_POST['pf_dd2'] > 0) {
+  $content .= zen_draw_form('productFinderform', FILENAME_DEFAULT, 'get', 'id="productFinderform"');
 } else {
-  $content .= '<span class="pf_selectbox_name">' . TEXT_YEAR . '</span>';
-  $content .= zen_draw_pull_down_menu('cPath22', $year, $value, 'id="select_year" class="pf_selectbox_text"');
+  $content .= zen_draw_form('productFinderform', '', 'post', 'id="productFinderform"');
 }
+$content .= '<div>' . "\n";
+$content .= '<div>' . "\n";
+if (isset($_POST['pf_dd1'])) {
+  $pf_dd1_selected = (int)$_POST['pf_dd1'];
+} elseif ($pf_cPaths) {
+  $pf_dd1_selected = $pf_cPaths['1'];
+}
+$content .= '<span id="pf_title">' . PF_TEXT_TITLE . '</span>' . "\n";
+$content .= '<br />';
+$content .= '<span class="pf_selectbox_name"><label for="pf_dd1">' . PF_TEXT_DD1 . '</label></span>' . "\n";
+$pf_dd1_array = pf_get_category_tree((int)$pf_base_category);
+$content .= zen_draw_pull_down_menu('pf_dd1', $pf_dd1_array, $pf_dd1_selected, 'id="pf_dd1" class="pf_selectbox_text"');
 $content .= '</div>' . "\n";
-$content .= '<div class="pf_noscript">';
-$content .= '<noscript>';
-if ($_POST['cPath'] != '') {
-  $value = $_POST['cPath'];
-}
+$content .= '<div class="pf_go">' . "\n";
+$content .= '<noscript>' . "\n";
 $content .= zen_image_submit(PF_NOSCRIPT_SUBMIT, PF_NOSCRIPT_SUBMIT_ALT);
-$content .= '</noscript>';
+$content .= '</noscript>' . "\n";
 $content .= '</div>' . "\n";
-$content .= '</div>';
-$content .= '</form>';
+$content .= '<div>' . "\n";
+
+if (isset($_POST['pf_dd2'])) {
+  $pf_dd2_selected = (int)$_POST['pf_dd2'];
+} elseif ($pf_cPaths) {
+  $pf_dd2_selected = $pf_cPaths['2'];
+}
+$content .= '<span class="pf_selectbox_name"><label for="pf_dd2">' . PF_TEXT_DD2 . '</label></span>' . "\n";
+$pf_dd2_array = pf_get_category_tree((int)$pf_dd1_selected);
+$content .= zen_draw_pull_down_menu('pf_dd2', $pf_dd2_array, $pf_dd2_selected, 'id="pf_dd2" class="pf_selectbox_text"');
+$content .= '</div>' . "\n";
+$content .= '<div class="pf_go">' . "\n";
+$content .= '<noscript>' . "\n";
+$content .= zen_image_submit(PF_NOSCRIPT_SUBMIT, PF_NOSCRIPT_SUBMIT_ALT);
+$content .= '</noscript>' . "\n";
+$content .= '</div>' . "\n";
+$content .= '<div>' . "\n";
+$content .= '<span class="pf_selectbox_name"><label for="cPath">' . PF_TEXT_DD3 . '</label></span>' . "\n";
+$pf_dd3_array = pf_get_category_tree((int)$pf_dd2_selected); //build the array of subcategories
+$pf_dd3_array[0]['id'] = '';
+foreach ($pf_dd3_array as $key => $subarray) {
+  if ($pf_dd3_array[$key]['id'] == 0) {
+    $pf_dd3_array[$key]['id'] = '';
+  } else {
+    $pf_dd3_array[$key]['id'] = $pf_base_category . '_' . $pf_dd1_selected . '_' . $pf_dd2_selected . '_' . $pf_dd3_array[$key]['id']; //prefix the dd3 cPath values with the pre-selected dd1_dd2 cPath values. Only needed for noscript submit button to pass as a GET parameter, the js version does the same thing and inserts the action into the form.
+  }
+}
+$content .= zen_draw_pull_down_menu('cPath', $pf_dd3_array, $pf_dd3_selected, 'id="cPath" class="pf_selectbox_text"');
+$content .= zen_draw_hidden_field('pf_base_category', $pf_base_category, 'id="pf_base_category" '); //to pass to js
+$content .= '</div>' . "\n";
+$content .= '<div class="pf_go">' . "\n";
+$content .= '<noscript>' . "\n";
+$content .= zen_image_submit(PF_NOSCRIPT_SUBMIT, PF_NOSCRIPT_SUBMIT_ALT);
+$content .= '</noscript>' . "\n";
+$content .= '</div>' . "\n";
+$content .= '</div>' . "\n";
+$content .= '</form>' . "\n";
 //<!--EOF Product Finder-->
 $content .= '</div>';
