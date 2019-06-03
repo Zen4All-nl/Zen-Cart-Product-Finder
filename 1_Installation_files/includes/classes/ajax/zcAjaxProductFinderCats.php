03c956ce8fc950e33a7be3f054a22699ae6925f8
Zen4All <info@zen4all.nl>
2019-06-03 12:14:22 +0200

    remove unused global

--- 1_Installation_files/includes/classes/ajax/zcAjaxProductFinderCats.php
+++ 1_Installation_files/includes/classes/ajax/zcAjaxProductFinderCats.php
@@ -1,19 +1,19 @@
 <?php
 
-/* Product Finder module
+/**
+ * Product Finder module
  * @version 1.1.0
  * @author Zen4All
  * @updated by TorVista & Zen4All
  * @copyright (c) 2014-2019, Zen4All
  * @license http://www.gnu.org/licenses/gpl.txt GNU General Public License V2.0
- * @version $Id: zcAjaxProductFinderCats.php 2019-05-25
+ * @version $Id: zcAjaxProductFinderCats.php 03-06-2019
  */
 
 class zcAjaxProductFinderCats extends base {
 
   public function getCategories()
   {
-    global $db;
     $valuesArray = pf_get_subcategories((int)$_POST['dd_cPath']); //the id of the category selected and submitted by the change in the dropdown
     return ([
       'valuesArray' => $valuesArray
